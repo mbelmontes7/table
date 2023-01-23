90686838b15ca99e68cdb251b2ca24317d187396
mbelmontes7 <you@example.com>
2023-01-22 18:17:35 -0600

    background image import as a svg

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
 function App() {
-    // const Learnyourmult:  = () => {
-        return (
-  <main class="flex min-h-screen w-full items-center justify-center bg-100 bg-purple-800">
-    <div class = "text-5xl h-48 w-15 content-center">Learn your multipication tables</div>
+  return (
+    <div className='h-screen w-screen bg-cover bg-no-repeat bg-[url("../public/assets/bg.svg")]'>
 
-     <div className="bg-purple-500"></div>
-  <button class="group relative h-12 w-56 -ml-10 overflow-hidden rounded-lg bg-purple text-xl shadow">
-    <div class="absolute inset-0 w-9 bg-purple-400 transition-all duration-[250ms] 
-    ease-out group-hover:w-full"></div>
-    <div class="relative text-black group-hover:text-white">Start here</div>
-  </button> {/* #im going to render here in this button */}
-</main>
-
-
-      
-
-
-    );
-  }
+     
+    <h1>Hello</h1>
+    </div>
+  );
+}
 export default App;
+
