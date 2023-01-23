4777129c62bb5e6dfd96de366d88617643296ce7
mbelmontes7 <you@example.com>
2023-01-22 23:06:55 -0600

    button needs to be center

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className='h-screen w-screen bg-cover bg-no-repeat bg-[url("../public/assets/bg.svg")]'>     
-    <h1>Hello</h1>
-    </div>
+    <div className='h-screen w-screen bg-cover bg-no-repeat bg-[url("../public/assets/bg.svg")]'>
+      <button class="w-50 h-30 items-center rounded-r-lg  bg-yellow-400  text-gray-800 font-bold p-6 uppercase border-yellow-500 border-t border-b border-r">Start here</button>
+</div>
+
   );
 }
 export default App;
