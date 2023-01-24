931650732c37f04dc8a01aecd9fe9b1f360550dd
mbelmontes7 <you@example.com>
2023-01-24 17:30:46 -0600

    button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className='h-screen w-screen bg-cover bg-no-repeat bg-[url("../public/assets/bg.svg")]'>
-      <button class="w-50 h-30 items-center rounded-r-lg  bg-yellow-400  text-gray-800 font-bold p-6 uppercase border-yellow-500 border-t border-b border-r">Start here</button>
+    <div className='h-55 w-66  bg-no-repeat bg-[url("../public/assets/button.svg")]'></div>
 </div>
 
   );
