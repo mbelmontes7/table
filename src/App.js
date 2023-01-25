e9208b04c20278a5418ded76b6ea53490a8534ab
mbelmontes7 <you@example.com>
2023-01-24 23:18:19 -0600

    Button has being made. > Render 2morrow

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+ import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
 import './App.css';
+import button from './icons/button.svg';
+
 
 function App() {
   return (
     <div className='h-screen w-screen bg-cover bg-no-repeat bg-[url("../public/assets/bg.svg")]'>
-    <div className='h-55 w-66  bg-no-repeat bg-[url("../public/assets/button.svg")]'></div>
+    <img src={button} alt='button'/>
 </div>
 
   );
